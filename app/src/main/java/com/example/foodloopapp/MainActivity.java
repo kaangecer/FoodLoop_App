f0d61f98f04778e402b4a9d763c0111f550ff106
kaangecer <jane.doe@hwr-berlin.de>
2026-01-25 16:17:11 +0100

    added navbar functionality

--- app/src/main/java/com/example/foodloopapp/MainActivity.java
+++ app/src/main/java/com/example/foodloopapp/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.foodloopapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+        }
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selected = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                selected = new HomeFragment();
+            } else if (id == R.id.nav_products) {
+                selected = new ProductsFragment();
+            } else if (id == R.id.nav_producers) {
+                selected = new ProducersFragment();
+            } else if (id == R.id.nav_maps) {
+                selected = new MapsFragment();
+            } else if (id == R.id.nav_cart) {
+                selected = new CartFragment();
+            }
+
+            if (selected != null) {
+                loadFragment(selected);
+                return true;
+            }
+            return false;
         });
     }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_host_container, fragment)
+                .commit();
+    }
+
 }
