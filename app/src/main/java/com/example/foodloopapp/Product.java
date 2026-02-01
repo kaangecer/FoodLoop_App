acc497c3ec238781c6e41a003e24144f62ddff4b
DiegoJahndorf <diego.jahndorf@live.com>
2026-02-01 02:06:01 +0100

    Produkt-Datenmodell hinzufügen

---
+++ app/src/main/java/com/example/foodloopapp/Product.java
@@ -0,0 +1,12 @@
+package com.example.foodloopapp;
+
+public class Product {
+    public final String name;
+    public final String price;
+
+    public Product(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
